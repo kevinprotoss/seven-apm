34368349321859ee7f73164f9f1bb3ccf9bd2d34
Junxiang Wei <kevinprotoss.wei@googlemail.com>
2018-08-27 10:38:29 +0000

    update majors.json change tutors page

--- src/app/tutors/tutors.component.ts
+++ src/app/tutors/tutors.component.ts
@@ -8,11 +8,23 @@ import {  trigger,  state,  style,  animate,  transition } from '@angular/animat
   animations: [
     trigger('fadeInOut', [
       transition(':enter', [   // :enter is alias to 'void => *'
-        style({opacity:0}),
-        animate(1000, style({opacity:1})) 
+        style({
+          opacity: 0, 
+          height: '*',
+          overflow: 'hidden'
+        }),
+        animate(1000, style({
+          opacity: 1,
+          height: '*',
+          overflow: 'hidden'
+        })) 
       ]),
       transition(':leave', [   // :leave is alias to '* => void'
-        animate(1000, style({opacity:0})) 
+        animate(1000, style({
+          opacity: 0,
+          height: '*',
+          overflow: 'hidden'
+        })) 
       ])
     ])
   ]
@@ -23,5 +35,16 @@ export class TutorsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      slidesPerColumn: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+      },
+    });
   }
 }
